d96fdedf7f64f352eee66840157fa85eb01e41f4
windopper <sevenstarpleiades@gmail.com>
2023-09-07 13:21:56 +0000

    change transform controls to vanilla js

--- prismstudio/src/prism/components/canvas/PrismTransformControls.tsx
+++ prismstudio/src/prism/components/canvas/PrismTransformControls.tsx
@@ -1,12 +1,9 @@
-import { TransformControls } from "@react-three/drei";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Mesh } from "three";
 import { RootState } from "store";
 import useTransformControls from "prism/hooks/useTransformControls";
 import useTransformControlEvent from "prism/hooks/useTransformControlEvent";
-import { TRANSLATION_SNAP } from "prism/constants";
-import { useThree } from "@react-three/fiber";
 
 export interface PrismTransformControlsProps {
   elementRefs: React.MutableRefObject<Map<string, Mesh>>;
@@ -15,9 +12,6 @@ export interface PrismTransformControlsProps {
 const PrismTransformControls = React.memo(
   (props: PrismTransformControlsProps) => {
     const { elementRefs } = props;
-    const controlRef = useRef<any>();
-    const { scene } = useThree();
-
     const { transformControlsMode } = useSelector(
       (state: RootState) => state.prismSlice
     );
@@ -32,12 +26,20 @@ const PrismTransformControls = React.memo(
 
     useEffect(() => {
       if (transformControls === undefined) return;
-      
+      transformControls.addEventListener('mouseDown', stopOrbitControls);
+      transformControls.addEventListener('mouseUp', startOrbitControls);
       transformControls?.addEventListener('dragging-changed', onDraggingChanged);
       return () => {
+        transformControls.removeEventListener('mouseDown', stopOrbitControls);
+        transformControls.removeEventListener('mouseUp', startOrbitControls);
         transformControls?.removeEventListener('dragging-changed', onDraggingChanged);
       }
     }, [transformControls]);
+
+    useEffect(() => {
+      if (transformControls === undefined) return;
+      transformControls.setMode(transformControlsMode);
+    }, [transformControlsMode])
 
     return null;
   }
